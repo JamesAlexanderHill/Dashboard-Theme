0d0a392d37c47a04b96bceb942a8360224e9c45e
James Hill <jhill7177@gmail.com>
2020-05-21 13:15:17 +1000

    style sfix + functions fix

---
+++ functions.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Dashboard Theme functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package WordPress
+ * @subpackage Dashboard_Theme
+ * @since Dashboard Theme 1.0
+ */
