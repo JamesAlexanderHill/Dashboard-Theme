12a9c82b5930b31bf36d7b07ee6a95764d60f9a4
JamesAlexanderHill <jhill7177@gmail.com>
2020-05-21 18:09:36 +1000

    Update functions.php

--- functions.php
+++ functions.php
@@ -101,7 +101,7 @@ function lesson_custom_column_values( $column, $post_id ) {
     case 'lesson':
       $type = get_post_meta($post_id, 'type', true );
       $day = get_post_meta($post_id, 'day', true );
-      $time = get_post_meta($post_id, 'time', true );
+      $time = the_time(get_post_meta($post_id, 'time', true ));
       echo "<a href='/wp-admin/post.php?post=" . $post_id . "&action=edit'>" . $type . " | " . $day . " - " . $time . "</a>";
       break;
     case 'coach':
