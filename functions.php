e6b2f61096b69ff98f9963adb88f8a95520ec196
James Hill <jhill7177@gmail.com>
2020-05-21 13:23:56 +1000

    Update functions.php

--- functions.php
+++ functions.php
@@ -8,3 +8,16 @@
  * @subpackage Dashboard_Theme
  * @since Dashboard Theme 1.0
  */
+
+//Edit Roles for users
+ function wps_add_role() {
+   add_role( 'coach', 'Coach', array('read', 'edit_posts'));
+ }
+ add_action( 'init', 'wps_add_role' );
+ function wps_remove_role() {
+     remove_role( 'editor' );
+     remove_role( 'author' );
+     remove_role( 'contributor' );
+     remove_role( 'subscriber' );
+ }
+ add_action( 'init', 'wps_remove_role' );
